cf098eaf547715845ed7609438108a092a08afc9
Fabrice Pargny <fabrice.pargny@gmail.com>
2025-05-01 14:21:25 +0200

    Add masonry

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 import jquery from 'jquery';
+import Masonry from 'masonry-layout';
