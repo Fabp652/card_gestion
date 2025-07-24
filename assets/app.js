f39636a41bec1d733028eb21240ee77ec1753e4f
Fabrice Pargny <fabrice.pargny@gmail.com>
2025-07-24 16:00:10 +0200

    Add tooltip to btn

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.scss';
 import jquery from 'jquery';
 import Masonry from 'masonry-layout';
+import { Tooltip } from 'bootstrap';
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
--- assets/controllers/filter_controller.js
+++ assets/controllers/filter_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
-import { Tooltip } from 'bootstrap';
 
 export default class extends Controller {
     connect() {
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
-
         $(document).on('change', 'tr th input, tr th select', function (e) {
             $('#formFilter').trigger('submit');
         });
